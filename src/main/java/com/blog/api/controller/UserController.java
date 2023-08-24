15c7e54129ac820e185444c026bf0c134492d5c8
ranjanmehto6 <ranjanmehto6@gmail.com>
2023-08-24 22:54:37 +0530

    Second commit

--- src/main/java/com/blog/api/controller/UserController.java
+++ src/main/java/com/blog/api/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.blog.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +27,16 @@ public class UserController {
 		UserDto createUser = this.userService.createUser(userDto);
 		return new ResponseEntity<>(createUser,HttpStatus.CREATED);
 	
+	}
+	
+	
+	@GetMapping("/list")
+	public List<UserDto> getAllUsers(){
+		
+		List<UserDto> allUser = this.userService.getAllUser();
+		
+		return allUser;
+		
 	}
 
 }
